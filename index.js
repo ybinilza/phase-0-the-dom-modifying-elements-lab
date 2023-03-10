f65f83a0fd43519a9c702079091a41ff8a475576
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-10 17:21:22 -0500

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id='victory';
+newHeader.textContent='BINIL THOMAS is the champion';
+document.body.append(newHeader);
+
+
+
